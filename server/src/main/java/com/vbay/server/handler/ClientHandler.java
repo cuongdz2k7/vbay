3a281cd1d9520bc0234af2d739600dbfd7f47521
NguyenDuyHung_CS6 <hungnguyen030962@gmail.com>
2026-03-26 01:04:31 +0700

    Commit

--- server/src/main/java/com/vbay/server/handler/ClientHandler.java
+++ server/src/main/java/com/vbay/server/handler/ClientHandler.java
@@ -7,40 +7,42 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
-    private final Socket socket; 
+    private final Socket socket;
 
-    //Constructor
+    // Constructor
     public ClientHandler(Socket socket) {
         this.socket = socket;
     }
+
     @Override
     public void run() {
-        System.out.println("Client connected: " +socket.getInetAddress().getHostAddress());
+        System.out.println("Client connected: " + socket.getInetAddress().getHostAddress());
+
         try (
             Socket clientSocket = socket;
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // client gui request
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true) // client nhan response
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
         ) {
             String line;
+
             while ((line = in.readLine()) != null) {
-                if (line.trim().isEmpty()){
+                if (line.trim().isEmpty()) {
                     System.out.println("Empty request");
                     continue;
                 }
-                else if ("exit".equalsIgnoreCase(line)){
+
+                if ("exit".equalsIgnoreCase(line)) {
                     out.println("Exit current socket");
                     break;
                 }
-                else {
-                    System.out.println("Received line: " + line);
-                    out.println("Echo: " + line);
-                }
+
+                System.out.println("Received line: " + line);
+                out.println("Echo: " + line);
             }
         } catch (IOException exception) {
             System.err.println("Client connection error: " + exception.getMessage());
-        }
-        finally{
-            System.out.println("Client disconnected: " +socket.getInetAddress().getHostAddress());
+        } finally {
+            System.out.println("Client disconnected: " + socket.getInetAddress().getHostAddress());
         }
     }
 }
