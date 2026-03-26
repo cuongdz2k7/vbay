f06d637130b3f4ce741b65fb9e7d48ed0561246b
NguyenDuyHung_CS6 <hungnguyen030962@gmail.com>
2026-03-27 00:19:09 +0700

    Debugging _Hung

--- server/src/main/java/com/vbay/server/Model/User.java
+++ server/src/main/java/com/vbay/server/Model/User.java
@@ -1,7 +1,6 @@
 package com.vbay.server.Model;
 import com.vbay.shared.enums.Position;
 import com.vbay.shared.enums.shared_status.UserStatus;
-
 import java.time.LocalDate;
 
 public class User {
--- shared/src/main/java/com/vbay/shared/enums/shared_request_type/AuctionProductRequestType.java
+++
@@ -1,9 +0,0 @@
-package com.vbay.shared.enums.shared_request_type;
-
-public enum AuctionProductRequestType {
-    GET_AUCTION_LIST,
-    GET_AUCTION_DETAIL,
-    SEARCH_AUCTION,
-    FILTER_AUCTION
-}
-
