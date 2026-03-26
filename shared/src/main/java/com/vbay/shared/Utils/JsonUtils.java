810f587fa58fa266c6ab0521d2220ddc66da3b54
Cuong <cuongscp049@gmail.com>
2026-03-26 23:45:44 +0700

    fafa

--- shared/src/main/java/com/vbay/shared/Utils/JsonUtils.java
+++ shared/src/main/java/com/vbay/shared/Utils/JsonUtils.java
@@ -1,18 +1,15 @@
 package com.vbay.shared.Utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
 public class JsonUtils { 
     private static final Gson gson = new Gson();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static String toJson(Object obj) {
         try {
-            return objectMapper.writeValueAsString(obj);
-        } catch (JsonProcessingException e) {
+            return gson.toJson(obj);
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -20,8 +17,8 @@ public class JsonUtils {
 
     public static <T> T fromJson(String json, Class<T> clazz) {
         try {
-            return objectMapper.readValue(json, clazz);
-        } catch (JsonSyntaxException | JsonProcessingException e) {
+            return gson.fromJson(json, clazz);
+        } catch (JsonSyntaxException e) {
             e.printStackTrace();
             return null;
         }
