dcd2ade17b33595d23d948b634c01c7b77acb202
NguyenDuyHung_CS6 <hungnguyen030962@gmail.com>
2026-04-03 22:25:26 +0700

    Commit

--- client/src/main/java/com/vbay/MainApp.java
+++ client/src/main/java/com/vbay/MainApp.java
@@ -1,20 +1,22 @@
 package com.vbay;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import com.vbay.network.SocketClient;
 import com.vbay.shared.enums.RequestType;
 import com.vbay.shared.protocol.Request;
-import com.vbay.ui.JavaFXApplication;
+import com.vbay.ui.scene.SceneManager;
 
-public class MainApp {
+import atlantafx.base.theme.PrimerLight;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    /**
-     * Set up JavaFX Application and enable window rendering.
-     */
-    public static void initUserInterface(String[] args) {
-        JavaFXApplication.launchApplication(args);
-    }
+public class MainApp extends Application{
+
 
     public static void initSocketClient() {
         SocketClient client = SocketClient.getClient();
@@ -37,9 +39,27 @@ public class MainApp {
         }
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
+        Parent loader = FXMLLoader.load(
+            Objects.requireNonNull(getClass().getResource("/jfx/scene/Authentication.fxml")));
+        Scene scene = new Scene(loader);
+        scene.getStylesheets().add(
+            Objects.requireNonNull(getClass().getResource("/jfx/css/app.css")).toExternalForm()
+        );
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("VBay");
+        primaryStage.setMinWidth(430);
+        primaryStage.setMinHeight(720);
+        primaryStage.show();
+
+        SceneManager.setStage(primaryStage);
+    }
+
     public static void main(String[] args) {
 //        initSocketClient();
-        initUserInterface(args);
+        launch(args);
     }
 
 }
