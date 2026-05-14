a51028acc50870f95575085a9214b0fc7bba6d4c
Cuong <cuongscp049@gmail.com>
2026-05-14 12:50:10 +0700

    Revert "Merge branch 'cuongdz2k7' of https://github.com/cuongdz2k7/vbay into cuongdz2k7"
    This reverts commit 15fe778896479c317eb38f4a1b3a837f4c66ada8, reversing
    changes made to 5e1ea0325c0434b7a60027ede6db9e3069fe2bf0.

--- client/src/main/java/com/vbay/ui/scene_ui/controller/home/HomeController.java
+++ client/src/main/java/com/vbay/ui/scene_ui/controller/home/HomeController.java
@@ -407,7 +407,7 @@ public class HomeController {
     }
 
     @FXML
-    private void handleYourInventory(ActionEvent event) {
+    private void handleCreateAuctions(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(CREATE_AUCTION_VIEW));
             BorderPane createAuctionRoot = loader.load();
